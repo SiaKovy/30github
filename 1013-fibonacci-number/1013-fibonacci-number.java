66121a268b6a080592c5da0d826958f17f355ac3
SeilTurd <136038015+SiaKovy@users.noreply.github.com>
2025-05-01 21:08:59 -0400

    Time: 0 ms (100%), Space: 40.1 MB (96.48%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,13 @@
 class Solution {
     public int fib(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return fib(n - 1) + fib(n - 2);
-}
+        if(n == 0) return 0;
+        int a = 0; 
+        int b = 1;
+        for(int i = 2; i <= n; i++){
+            int temp = a+b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
 }
